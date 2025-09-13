b50ecb1d304482d97e038c71f646f2a2b048e4a5
Nils Emmenegger <61921611+nils-emmenegger@users.noreply.github.com>
2025-09-13 19:06:09 -0400

    /chat endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,27 @@
-from flask import Flask
+import os
+from flask import Flask, request
+import cohere
+from dotenv import load_dotenv
+
+load_dotenv()
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "hello world"
+co = cohere.ClientV2(
+    os.environ["COHERE_API_KEY"]
+)
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    messages = request.json["messages"]
+
+    response = co.chat(
+        model="command-a-03-2025",
+        messages=messages,
+    )
+
+    return {"response": response.message.content[0].text}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    print("starting")
+    app.run(port=8000, debug=True)
